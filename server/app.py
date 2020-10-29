cb1b37821b559e16b07c247c89fa0631f23ec68e
yy <yy@qixin.ai>
2020-10-30 00:45:08 +0800

    basic restful api

--- server/app.py
+++ server/app.py
@@ -4,17 +4,21 @@ from sys import prefix
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', prefix + os.path.join(app.root_path, 'data.db'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv
 WIN = sys.platform.startswith('win')
 if WIN:
     prefix = 'sqlite:///'
 else:
     prefix = 'sqlite:////'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', prefix + os.path.join(app.root_path, 'data.db'))
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 class TextFlow(db.Model):
@@ -39,4 +43,39 @@ class Project(db.Model):
     project_name = db.Column(db.Text)
     project_type = db.Column(db.Integer)
     src_language = db.Column(db.String(length=10))
+
+
+tfs = ["abc", "def", "ghi"]
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+class GetFile(Resource):
+    def get(self, file_id):
+        return tfs
+
+
+class PostFile(Resource):
+    def post(self, project_id):
+        pass
+
+
+class ProjectResource(Resource):
+    def get(self, project_id):
+        pass
+    
+    def post(self):
+        pass
+
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(GetFile, "/api/file/get/<string:file_id>")
+api.add_resource(PostFile, "api/file/upload/<string:project_id>")
+
+
+
+
 
